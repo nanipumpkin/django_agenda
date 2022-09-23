3913e68881965f86e8ea734f29114678b36b8921
Marianne Costa Avalone <marianne.avalone@gmail.com>
2022-09-23 15:25:26 -0300

    Added date filter pt-br, http404 response, adjusted settings configs

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import datetime
 # Create your models here.
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
@@ -20,3 +20,9 @@ class Evento(models.Model):
     
     def get_data_input(self):
         return self.data.strftime('%Y-%m-%dT%H:%M')
+    
+    def get_evento_atrasado(self):
+        if self.data < datetime.now():
+            return True
+        else:
+            return False
